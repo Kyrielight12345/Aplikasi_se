b7f4633be44e9745a08919e68ffc0fdcf2e193da
“Kyrielight12345” <Kyrielight12345@gmail.com>
2023-07-09 14:37:04 +0700

    kepsek new 98%

--- kelompok_pse (5).sql
+++ kelompok_pse (5).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 08, 2023 at 05:24 AM
+-- Generation Time: Jul 09, 2023 at 09:36 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.27
 
@@ -24,15 +24,65 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `data_pelanggaran`
+--
+
+CREATE TABLE `data_pelanggaran` (
+  `id` int(11) NOT NULL,
+  `nis` varchar(10) NOT NULL,
+  `jenis` varchar(100) NOT NULL,
+  `poin` int(11) NOT NULL,
+  `tanggal` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `data_pelanggaran`
+--
+
+INSERT INTO `data_pelanggaran` (`id`, `nis`, `jenis`, `poin`, `tanggal`) VALUES
+(1, '1255', 'membolos', 50, '2023-07-29'),
+(2, '1255', 'membolos', 50, '2023-07-08'),
+(3, '1212', 'membolos', 50, '2023-07-27'),
+(4, '1212', 'membolos', 70, '2023-07-28'),
+(5, '10221', 'membolos', 50, '2023-07-28'),
+(6, '1212', 'membolos', 50, '2023-07-21');
+
+--
+-- Triggers `data_pelanggaran`
+--
+DELIMITER $$
+CREATE TRIGGER `nama_trigger` AFTER INSERT ON `data_pelanggaran` FOR EACH ROW BEGIN
+    DECLARE var1 VARCHAR(255);
+    SELECT nis INTO var1 FROM pelanggaran WHERE nis = NEW.nis;
+
+    IF var1 IS NULL THEN
+        INSERT INTO pelanggaran (nis, total_poin) VALUES (NEW.nis,NEW.poin);
+    ELSE
+        UPDATE pelanggaran SET total_poin = new.poin + total_poin WHERE nis = NEW.nis;
+    END IF;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `dispensasi`
 --
 
 CREATE TABLE `dispensasi` (
-  `kode_dispen` varchar(12) NOT NULL,
+  `kode_dispen` int(11) NOT NULL,
   `nis` varchar(10) NOT NULL,
   `tanggal` date NOT NULL,
   `keterangan` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `dispensasi`
+--
+
+INSERT INTO `dispensasi` (`kode_dispen`, `nis`, `tanggal`, `keterangan`) VALUES
+(1, '10221', '2023-07-21', 'bolos');
 
 -- --------------------------------------------------------
 
@@ -51,7 +101,7 @@ CREATE TABLE `guru_dan_staf` (
   `jabatan` enum('Admin','Guru','karyawan') NOT NULL,
   `foto` varchar(255) NOT NULL,
   `sk` varchar(100) NOT NULL,
-  `status` varchar(100) NOT NULL
+  `status` enum('Honorer','PNS') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -59,9 +109,10 @@ CREATE TABLE `guru_dan_staf` (
 --
 
 INSERT INTO `guru_dan_staf` (`id_guru`, `nama_guru`, `jenis_kelamin`, `alamat`, `no_hp`, `tempat_lahir`, `tgl_lahir`, `jabatan`, `foto`, `sk`, `status`) VALUES
-('111', 'sumi', 'Laki-Laki', '', '', '', '0000-00-00', 'Guru', '', '', ''),
-('121', 'Guntur', 'Laki-Laki', 'batang', '345678', 'btg', '2023-03-02', 'Admin', '', '', ''),
-('123', 'Supriyanto', 'Laki-Laki', '', '', '', '0000-00-00', 'Guru', '1684981264_e5455e6d1adb2630d566.jpg', '1687397552_754d0128f5301123d2a7.png', 'PNS');
+('111', 'sumi', 'Laki-Laki', '', '', '', '0000-00-00', 'Guru', '', '', 'Honorer'),
+('121', 'Guntur', 'Laki-Laki', 'batang', '345678', 'btg', '2023-03-02', 'Admin', '', '', 'Honorer'),
+('123', 'Supriyanto', 'Laki-Laki', '', '', '', '0000-00-00', 'Guru', '1684981264_e5455e6d1adb2630d566.jpg', '1687397552_754d0128f5301123d2a7.png', 'PNS'),
+('V23', 'view', 'Perempuan', '', '', '', '0000-00-00', 'Admin', '', '', 'Honorer');
 
 -- --------------------------------------------------------
 
@@ -71,7 +122,7 @@ INSERT INTO `guru_dan_staf` (`id_guru`, `nama_guru`, `jenis_kelamin`, `alamat`, 
 
 CREATE TABLE `jadwal` (
   `id_jadwal` varchar(10) NOT NULL,
-  `mapel` varchar(50) NOT NULL,
+  `id_mapel` varchar(10) NOT NULL,
   `id_ruang` varchar(10) NOT NULL,
   `id_guru` varchar(20) NOT NULL,
   `hari` varchar(100) NOT NULL,
@@ -83,12 +134,8 @@ CREATE TABLE `jadwal` (
 -- Dumping data for table `jadwal`
 --
 
-INSERT INTO `jadwal` (`id_jadwal`, `mapel`, `id_ruang`, `id_guru`, `hari`, `jam_awal`, `jam_akhir`) VALUES
-('A-01', 'Indonesia', 'F24', '123', 'senin', '08:00:00', '10:00:00'),
-('A-02', 'Indonesia', 'F13', '123', 'selasa', '07:00:00', '09:00:00'),
-('A-03', 'Indonesia', 'F14', '123', 'rabu', '10:00:00', '12:00:00'),
-('I-12', 'indonesia', 'A12', '123', 'kamis', '07:00:00', '09:00:00'),
-('S-02', 'Indonesia', 'F25', '123', 'jumat', '13:00:00', '15:00:00');
+INSERT INTO `jadwal` (`id_jadwal`, `id_mapel`, `id_ruang`, `id_guru`, `hari`, `jam_awal`, `jam_akhir`) VALUES
+('I-12', 'C23', 'F13', '123', 'Rabu', '06:05:00', '06:05:00');
 
 -- --------------------------------------------------------
 
@@ -101,6 +148,13 @@ CREATE TABLE `kalender` (
   `tanggal` date DEFAULT NULL,
   `keterangan` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kalender`
+--
+
+INSERT INTO `kalender` (`id_kalender`, `tanggal`, `keterangan`) VALUES
+(1, '2023-07-28', 'Hari Raya Idul Adha');
 
 -- --------------------------------------------------------
 
@@ -138,6 +192,17 @@ CREATE TABLE `kelola_ujian` (
   `jam_keluar` time DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `kelola_ujian`
+--
+
+INSERT INTO `kelola_ujian` (`id`, `id_guru`, `id_mapel`, `id_ruang`, `tanggal`, `jam_masuk`, `jam_keluar`) VALUES
+(1, '10250', 'C11', 'A12', '2023-07-05', '22:17:00', '16:23:00'),
+(14, '10253', 'C22', 'A15', '2023-08-04', '16:36:00', '19:31:00'),
+(15, '10254', 'C22', 'A16', '2023-07-07', '13:00:00', '13:30:00'),
+(16, '10253', 'C11', 'A12', '2023-07-07', '14:30:00', '15:00:00'),
+(17, '123', 'C23', 'A12', '2023-07-27', '12:40:00', '12:40:00');
+
 -- --------------------------------------------------------
 
 --
@@ -145,10 +210,18 @@ CREATE TABLE `kelola_ujian` (
 --
 
 CREATE TABLE `konseling` (
-  `kode_konseling` varchar(12) NOT NULL,
+  `kode_konseling` int(11) NOT NULL,
   `nis` varchar(10) NOT NULL,
-  `tanggal` date NOT NULL
+  `tanggal` date NOT NULL,
+  `perihal` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `konseling`
+--
+
+INSERT INTO `konseling` (`kode_konseling`, `nis`, `tanggal`, `perihal`) VALUES
+(1, '1212', '2023-07-29', 'masalah keluarga');
 
 -- --------------------------------------------------------
 
@@ -157,10 +230,17 @@ CREATE TABLE `konseling` (
 --
 
 CREATE TABLE `kurikulum` (
-  `id_kurikulum` varchar(10) NOT NULL,
-  `kompetensi dasar` varchar(50) NOT NULL,
+  `id_kurikulum` varchar(11) NOT NULL,
+  `kompetensi_dasar` varchar(50) NOT NULL,
   `deskripsi` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kurikulum`
+--
+
+INSERT INTO `kurikulum` (`id_kurikulum`, `kompetensi_dasar`, `deskripsi`) VALUES
+('KD-13', 'CI4', 'membuat cI4');
 
 -- --------------------------------------------------------
 
@@ -230,9 +310,10 @@ CREATE TABLE `nilai` (
 
 INSERT INTO `nilai` (`id_nilai`, `nis`, `semester`, `nilai`) VALUES
 (1, '10242', 1, 6.38462),
-(2, '1255', 2, 0),
+(2, '1255', 2, 4.84615),
 (3, '1212', 1, 0),
-(4, '10221', 1, 0);
+(4, '10221', 1, 0),
+(5, '1121', 1, 2.69231);
 
 --
 -- Triggers `nilai`
@@ -272,9 +353,10 @@ CREATE TABLE `nilai_all_guru` (
 
 INSERT INTO `nilai_all_guru` (`nis`, `semester`, `indonesia`, `inggris`, `pai`, `ppkn`, `mtk`, `kimia`, `fisika`, `jawa`, `kwu`, `penjas`, `jurusan_1`, `jurusan_2`, `jurusan_3`) VALUES
 ('10242', 1, 83, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-('1255', 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+('1255', 2, 63, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
 ('1212', 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-('10221', 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+('10221', 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+('1121', 1, 35, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 --
 -- Triggers `nilai_all_guru`
@@ -309,9 +391,10 @@ CREATE TABLE `nilai_mapel` (
 
 INSERT INTO `nilai_mapel` (`id_mapel`, `nis`, `id_guru`, `tugas1`, `tugas2`, `tugas3`, `tugas4`, `uts`, `uas`, `keterangan_rata`) VALUES
 (1, '10242', '123', 70, 80, 80, 70, 100, 100, 'Terpenuhi'),
-(2, '1255', '123', 0, 0, 0, 0, 0, 0, 'Tidak Terpenuhi'),
+(2, '1255', '123', 90, 50, 60, 50, 90, 40, 'Tidak Terpenuhi'),
 (3, '1212', '123', 0, 0, 0, 0, 0, 0, 'Tidak Terpenuhi'),
-(4, '10221', '123', 0, 0, 0, 0, 0, 0, 'Tidak Terpenuhi');
+(4, '10221', '123', 0, 0, 0, 0, 0, 0, 'Tidak Terpenuhi'),
+(5, '1121', '123', 70, 70, 70, 0, 0, 0, 'Tidak Terpenuhi');
 
 -- --------------------------------------------------------
 
@@ -320,15 +403,19 @@ INSERT INTO `nilai_mapel` (`id_mapel`, `nis`, `id_guru`, `tugas1`, `tugas2`, `tu
 --
 
 CREATE TABLE `pelanggaran` (
-  `id_pelanggaran` varchar(11) NOT NULL,
-  `tanggal` date NOT NULL,
+  `id_pelanggaran` int(11) NOT NULL,
   `nis` varchar(10) NOT NULL,
-  `jenis` varchar(30) NOT NULL,
-  `poin` int(30) NOT NULL,
-  `total_poin` int(30) NOT NULL,
-  `tindak_lanjut` varchar(100) NOT NULL,
-  `keterangan` text NOT NULL
+  `total_poin` int(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pelanggaran`
+--
+
+INSERT INTO `pelanggaran` (`id_pelanggaran`, `nis`, `total_poin`) VALUES
+(1, '1255', 100),
+(2, '1212', 170),
+(3, '10221', 50);
 
 -- --------------------------------------------------------
 
@@ -337,6 +424,7 @@ CREATE TABLE `pelanggaran` (
 --
 
 CREATE TABLE `presensi` (
+  `id` int(11) NOT NULL,
   `nis` varchar(10) NOT NULL,
   `HADIR` int(11) NOT NULL,
   `IJIN` int(11) NOT NULL,
@@ -350,11 +438,12 @@ CREATE TABLE `presensi` (
 -- Dumping data for table `presensi`
 --
 
-INSERT INTO `presensi` (`nis`, `HADIR`, `IJIN`, `ALPHA`, `total`, `presentase`, `pertemuan`) VALUES
-('10242', 1, 0, 0, 1, 100, 1),
-('1255', 0, 0, 0, 0, 0, 0),
-('1212', 0, 0, 0, 0, 0, 0),
-('10221', 0, 0, 0, 0, 0, 0);
+INSERT INTO `presensi` (`id`, `nis`, `HADIR`, `IJIN`, `ALPHA`, `total`, `presentase`, `pertemuan`) VALUES
+(1, '10242', 1, 0, 0, 1, 100, 1),
+(2, '1255', 1, 0, 0, 1, 100, 1),
+(3, '1212', 1, 0, 0, 1, 100, 1),
+(4, '10221', 1, 0, 0, 1, 100, 1),
+(5, '1121', 0, 0, 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -375,7 +464,8 @@ INSERT INTO `presensi_total` (`nis`, `keterangan`) VALUES
 ('10242', 'hadir'),
 ('1255', 'hadir'),
 ('1212', 'hadir'),
-('10221', 'hadir');
+('10221', 'hadir'),
+('1121', 'hadir');
 
 --
 -- Triggers `presensi_total`
@@ -411,6 +501,7 @@ DELIMITER ;
 --
 
 CREATE TABLE `regalisir` (
+  `id` int(11) NOT NULL,
   `nis` varchar(10) NOT NULL,
   `file` varchar(100) NOT NULL,
   `status` enum('BELUM REGALISIR','SUDAH REGALISIR') NOT NULL
@@ -420,11 +511,11 @@ CREATE TABLE `regalisir` (
 -- Dumping data for table `regalisir`
 --
 
-INSERT INTO `regalisir` (`nis`, `file`, `status`) VALUES
-('1212', '1688767375_162e9efa79783b98def9.png', 'SUDAH REGALISIR'),
-('1255', '1688767402_073e54804888bf0c001a.png', 'SUDAH REGALISIR'),
-('10242', '1688767412_8d5af43a62ee6151e4ae.png', 'SUDAH REGALISIR'),
-('10221', '', 'BELUM REGALISIR');
+INSERT INTO `regalisir` (`id`, `nis`, `file`, `status`) VALUES
+(1, '1212', '1688767375_162e9efa79783b98def9.png', 'SUDAH REGALISIR'),
+(2, '1255', '1688767402_073e54804888bf0c001a.png', 'SUDAH REGALISIR'),
+(3, '10242', '1688767412_8d5af43a62ee6151e4ae.png', 'SUDAH REGALISIR'),
+(4, '10221', '', 'BELUM REGALISIR');
 
 -- --------------------------------------------------------
 
@@ -475,6 +566,7 @@ CREATE TABLE `siswa` (
 INSERT INTO `siswa` (`nis`, `nama_siswa`, `tempat_lahir`, `tgl_lahir`, `jenis_kelamin`, `agama`, `alamat`, `jurusan`, `no_hp`, `id_kelas`) VALUES
 ('10221', 'Dewi', 'batang', '2023-07-08', 'Laki-Laki', 'Kristen', 'werfg', 'PM', '123456789p', '10'),
 ('10242', 'GUNTUR', 'Batang', '2023-07-07', 'Laki-Laki', 'Islam', 'Lawangaji', 'TKJ', '0987654321', '10'),
+('1121', 'apep', 'Batang', '2023-07-09', 'Perempuan', 'Konghucu', 'pjhfsfghj.', 'TKJ', '123456789p', '10'),
 ('1212', 'Riska', 'Batang', '2023-07-07', 'Laki-Laki', 'Islam', 'Batang', 'AK', '098765432122', '10'),
 ('1255', 'Antok', 'batang', '2023-07-07', 'Perempuan', 'Islam', 'subah', 'TKJ', '098765432122', '11');
 
@@ -511,9 +603,15 @@ CREATE TABLE `surat` (
   `hari` varchar(20) NOT NULL,
   `tanggal` date NOT NULL,
   `waktu` varchar(20) NOT NULL,
-  `tempat` varchar(50) NOT NULL,
   `keperluan` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `surat`
+--
+
+INSERT INTO `surat` (`no_surat`, `hal`, `nis`, `hari`, `tanggal`, `waktu`, `keperluan`) VALUES
+('ss-12-12', 'sanksi berat', '1255', 'selasa', '2023-07-09', 'jam 6', 'keperluan tindak lanjut DO');
 
 -- --------------------------------------------------------
 
@@ -536,7 +634,7 @@ CREATE TABLE `surat_keluar` (
 --
 
 INSERT INTO `surat_keluar` (`id_surat`, `no_surat`, `pengirim`, `prihal`, `tgl_keluar`, `file`, `status`) VALUES
-(6, '11/ABC/X/2023', 'DINAS PENDIDIKAN ', 'PERMOHONAN hahha', '2023-06-15', '1687170499_f7415220ac4573ef3de8.jpg', 'Sudah Disposisi');
+(6, '11/ABC/X/2023', 'DINAS PENDIDIKAN ', 'PERMOHONAN hahha', '2023-06-15', '1687170499_f7415220ac4573ef3de8.jpg', 'Menunggu Disposisi');
 
 -- --------------------------------------------------------
 
@@ -559,8 +657,8 @@ CREATE TABLE `surat_masuk` (
 --
 
 INSERT INTO `surat_masuk` (`id_surat`, `no_surat`, `pengirim`, `prihal`, `tgl_masuk`, `file`, `status`) VALUES
-(1, '11/ABC/X/2023', 'DINAS PENDIDIKAN', 'UNDANGAN\\', '2012-02-11', '1688765534_c5925a3c26e12fac193e.png', 'Menunggu Disposisi'),
-(3, '12/DIL/II/2023', 'UKM PK', 'PROGRES 10', '2023-06-21', '1687321690_2eb710283166a54d0898.png', 'Sudah Disposisi');
+(1, '11/ABC/X/2023', 'DINAS PENDIDIKAN', 'UNDANGAN\\', '2012-02-11', '1688765534_c5925a3c26e12fac193e.png', 'Sudah Disposisi'),
+(3, '12/DIL/II/2023', 'UKM PK', 'PROGRES 10', '2023-06-21', '1687321690_2eb710283166a54d0898.png', 'Menunggu Disposisi');
 
 -- --------------------------------------------------------
 
@@ -572,7 +670,7 @@ CREATE TABLE `users` (
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `id_guru` varchar(20) NOT NULL,
-  `akses` enum('guru','admin','kepsek') NOT NULL,
+  `akses` enum('guru','admin','kepsek','bk','kurikulum') NOT NULL,
   `nama` varchar(100) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL
@@ -583,13 +681,22 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`username`, `password`, `id_guru`, `akses`, `nama`, `created_at`, `updated_at`) VALUES
-('coba', '123', '111', 'guru', 'ww', '2023-06-10 10:24:39', '2023-06-10 10:24:39'),
+('bk', '123', 'V23', 'bk', 'bk_new', '2023-07-08 21:33:03', '2023-07-08 21:41:18'),
 ('guntur', '123', '121', 'admin', 'guntur', '2023-05-24 12:46:07', '2023-05-24 12:46:07'),
-('guru', '123', '123', 'guru', 'Supriyanto', '2023-05-24 12:46:31', '2023-05-24 12:46:31');
+('guru', '123', '123', 'guru', 'Supriyanto', '2023-05-24 12:46:31', '2023-05-24 12:46:31'),
+('kepsek', '123', '111', 'kepsek', 'kepsek', '2023-06-10 10:24:39', '2023-06-10 10:24:39'),
+('kurikulum', '123', '111', 'kurikulum', 'Asih', '2023-07-09 05:35:23', '2023-07-09 05:35:23');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `data_pelanggaran`
+--
+ALTER TABLE `data_pelanggaran`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `nis` (`nis`);
 
 --
 -- Indexes for table `dispensasi`
@@ -610,7 +717,8 @@ ALTER TABLE `guru_dan_staf`
 ALTER TABLE `jadwal`
   ADD PRIMARY KEY (`id_jadwal`),
   ADD KEY `id_guru` (`id_guru`),
-  ADD KEY `id_ruang` (`id_ruang`);
+  ADD KEY `id_ruang` (`id_ruang`),
+  ADD KEY `id_mapel` (`id_mapel`);
 
 --
 -- Indexes for table `kalender`
@@ -637,6 +745,7 @@ ALTER TABLE `kelola_ujian`
 -- Indexes for table `konseling`
 --
 ALTER TABLE `konseling`
+  ADD PRIMARY KEY (`kode_konseling`),
   ADD KEY `nis` (`nis`);
 
 --
@@ -689,6 +798,7 @@ ALTER TABLE `pelanggaran`
 -- Indexes for table `presensi`
 --
 ALTER TABLE `presensi`
+  ADD PRIMARY KEY (`id`),
   ADD KEY `nis` (`nis`);
 
 --
@@ -701,6 +811,7 @@ ALTER TABLE `presensi_total`
 -- Indexes for table `regalisir`
 --
 ALTER TABLE `regalisir`
+  ADD PRIMARY KEY (`id`),
   ADD KEY `nis` (`nis`);
 
 --
@@ -747,22 +858,64 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `data_pelanggaran`
+--
+ALTER TABLE `data_pelanggaran`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `dispensasi`
+--
+ALTER TABLE `dispensasi`
+  MODIFY `kode_dispen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `kalender`
+--
+ALTER TABLE `kalender`
+  MODIFY `id_kalender` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `kelola_ujian`
 --
 ALTER TABLE `kelola_ujian`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `konseling`
+--
+ALTER TABLE `konseling`
+  MODIFY `kode_konseling` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `nilai`
 --
 ALTER TABLE `nilai`
-  MODIFY `id_nilai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_nilai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `nilai_mapel`
 --
 ALTER TABLE `nilai_mapel`
-  MODIFY `id_mapel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_mapel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `pelanggaran`
+--
+ALTER TABLE `pelanggaran`
+  MODIFY `id_pelanggaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `presensi`
+--
+ALTER TABLE `presensi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `regalisir`
+--
+ALTER TABLE `regalisir`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `surat_keluar`
@@ -781,6 +934,12 @@ ALTER TABLE `surat_masuk`
 --
 
 --
+-- Constraints for table `data_pelanggaran`
+--
+ALTER TABLE `data_pelanggaran`
+  ADD CONSTRAINT `data_pelanggaran_ibfk_1` FOREIGN KEY (`nis`) REFERENCES `siswa` (`nis`);
+
+--
 -- Constraints for table `dispensasi`
 --
 ALTER TABLE `dispensasi`
@@ -791,15 +950,8 @@ ALTER TABLE `dispensasi`
 --
 ALTER TABLE `jadwal`
   ADD CONSTRAINT `jadwal_ibfk_1` FOREIGN KEY (`id_guru`) REFERENCES `guru_dan_staf` (`id_guru`),
-  ADD CONSTRAINT `jadwal_ibfk_2` FOREIGN KEY (`id_ruang`) REFERENCES `ruang` (`id_ruang`);
-
---
--- Constraints for table `kelola_ujian`
---
-ALTER TABLE `kelola_ujian`
-  ADD CONSTRAINT `kelola_ujian_ibfk_1` FOREIGN KEY (`id_guru`) REFERENCES `guru_dan_staf` (`id_guru`),
-  ADD CONSTRAINT `kelola_ujian_ibfk_2` FOREIGN KEY (`id_mapel`) REFERENCES `mapel` (`id_mapel`),
-  ADD CONSTRAINT `kelola_ujian_ibfk_3` FOREIGN KEY (`id_ruang`) REFERENCES `ruang` (`id_ruang`);
+  ADD CONSTRAINT `jadwal_ibfk_2` FOREIGN KEY (`id_ruang`) REFERENCES `ruang` (`id_ruang`),
+  ADD CONSTRAINT `jadwal_ibfk_3` FOREIGN KEY (`id_mapel`) REFERENCES `mapel` (`id_mapel`);
 
 --
 -- Constraints for table `konseling`
